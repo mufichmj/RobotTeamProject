0371de64152543055db033f1a506f7f83705897a
watsonzr <watsonzr@rose-hulman.edu>
2018-05-16 10:34:05 -0400

    Final_Project watsonzr creating GUI

--- projects/watsonzr/pc_Final_Project.py
+++ projects/watsonzr/pc_Final_Project.py
@@ -14,9 +14,24 @@ class ComputerDelegate(object):
 
 
 def main():
-    cd = ComputerDelegate()
+
+    root = tkinter.Tk()
+    root.title('Zack Watson Final Project GUI')
+
+    main_frame = ttk.Frame(root, padding=20, relief='raised')
+    main_frame.grid()
+
+    sample_label = ttk.Label(main_frame, text="Zack Watson")
+    sample_label.grid(row=0, column=0)
+
+    display_location = ttk.Label(main_frame, text="Robot messages will display here")
+    display_location.grid(row=1, column=0)
+    
+    cd = ComputerDelegate(display_location)
     mqtt_client = com.MqttClient(cd)
     mqtt_client.connect_to_ev3()
+
+    root.mainloop()
 
 
 main()
