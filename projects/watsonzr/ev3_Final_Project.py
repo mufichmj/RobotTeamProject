221239aaf8b84a6a8949b4492bf59e1dfbc2f4c7
watsonzr <watsonzr@rose-hulman.edu>
2018-05-16 10:28:28 -0400

    Final_Project watsonzr establishing delegate class and starting mqtt

--- projects/watsonzr/ev3_Final_Project.py
+++ projects/watsonzr/ev3_Final_Project.py
@@ -4,3 +4,22 @@ import ev3dev as ev3
 import robot_controller as robo
 import time
 import mqtt_remote_method_calls as com
+
+
+class RobotDelegate(object):
+
+    def __init__(self):
+        self.robot = robo.Snatch3r()
+
+    def loop_forever(self):
+        self.robot.loop_forever()
+
+
+def main():
+    rd = RobotDelegate()
+    mqtt_client = com.MqttClient(rd)
+    mqtt_client.connect_to_pc()
+    rd.loop_forever()
+
+
+main()
--- projects/watsonzr/pc_Final_Project.py
+++ projects/watsonzr/pc_Final_Project.py
@@ -5,3 +5,18 @@ import time
 import mqtt_remote_method_calls as com
 import tkinter
 from tkinter import ttk
+
+
+class ComputerDelegate(object):
+
+    def __init__(self, location_to_display):
+        self.place_to_display = location_to_display
+
+
+def main():
+    cd = ComputerDelegate()
+    mqtt_client = com.MqttClient(cd)
+    mqtt_client.connect_to_ev3()
+
+
+main()
