915fbd31754e483727ea0760a5e3f248dc07180a
watsonzr <watsonzr@rose-hulman.edu>
2018-05-17 11:09:28 -0400

    final_project dances

--- projects/watsonzr/ev3_Final_Project.py
+++ projects/watsonzr/ev3_Final_Project.py
@@ -28,7 +28,7 @@ class RobotDelegate(object):
         time.sleep(0.75)
         self.robot.stop()
         self.robot.arm_part_up()
-        time.sleep(3)
+        time.sleep(2)
         self.robot.left(500, 500)
         time.sleep(0.5)
         self.robot.right(500, 500)
