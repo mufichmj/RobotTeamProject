955d6f9d874dc7dee5f800220a00c20a02fcce56
mufichmj <mufichmj@rose-hulman.edu>
2018-05-16 20:52:58 -0400

    mufichmj

--- projects/Mariah/Mariah_Project_PC.py
+++ projects/Mariah/Mariah_Project_PC.py
@@ -109,19 +109,19 @@ def send_cones(mqtt_client, number_of_cones_entry):
 
 def send_left(mqtt_client, left_speed_entry, right_speed_entry):
     print("left")
-    mqtt_client.send_message("pass_the_ball", [-int(left_speed_entry.get()),
+    mqtt_client.send_message("shoot_the_ball", [-int(left_speed_entry.get()),
                                                int(right_speed_entry.get())])
 
 
 def send_right(mqtt_client, left_speed_entry, right_speed_entry):
     print("right")
-    mqtt_client.send_message("pass_the_ball", [int(left_speed_entry.get()),
+    mqtt_client.send_message("shoot_the_ball", [int(left_speed_entry.get()),
                                        -int(right_speed_entry.get())])
 
 
 def send_forward(mqtt_client, left_speed_entry, right_speed_entry):
     print("forward")
-    mqtt_client.send_message("shoot", [int(left_speed_entry.get()),
+    mqtt_client.send_message("shoot_soccer_ball", [int(left_speed_entry.get()),
                                            int(right_speed_entry.get())])
 
 
@@ -135,6 +135,9 @@ def send_up(mqtt_client):
 def send_down(mqtt_client):
     print("arm_down")
     mqtt_client.send_message("yellow_card")
+
+
+
 
 
 def send_back(mqtt_client, left_speed_entry, right_speed_entry):
