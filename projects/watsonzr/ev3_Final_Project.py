7c1e22942e015c7e0370ab9c7ff4f46c45a73d6a
watsonzr <watsonzr@rose-hulman.edu>
2018-05-18 10:29:30 -0400

    final_project dances

--- projects/watsonzr/ev3_Final_Project.py
+++ projects/watsonzr/ev3_Final_Project.py
@@ -15,6 +15,7 @@ class RobotDelegate(object):
         self.robot.loop_forever()
 
     def moonwalk(self):
+        send_pc_message('Moonwalking!')
         self.robot.left_motor.run_forever(speed_sp=-900)
         time.sleep(0.25)
         self.robot.stop()
@@ -28,8 +29,10 @@ class RobotDelegate(object):
         self.robot.right_motor.run_forever(speed_sp=-900)
         time.sleep(0.25)
         self.robot.stop()
+        send_pc_message('Waiting...')
 
     def waltz(self):
+        send_pc_message('Waltzing!')
         for _ in range(3):
             self.robot.left_motor.run_forever(speed_sp=400)
             time.sleep(0.75)
@@ -49,15 +52,19 @@ class RobotDelegate(object):
             self.robot.right_motor.run_forever(speed_sp=-400)
             time.sleep(0.5)
             self.robot.stop()
+        send_pc_message('Waiting...')
 
     def square_dance(self):
+        send_pc_message('Do Si Do-ing!')
         for _ in range(4):
             self.robot.forward(500, 500)
             time.sleep(3)
             self.robot.stop()
             self.robot.turn_left_degrees(90)
+        send_pc_message('Waiting...')
 
     def whip_nae_nae(self):
+        send_pc_message('Watch Me!')
         self.robot.left(500, 500)
         time.sleep(0.5)
         self.robot.stop()
@@ -69,9 +76,12 @@ class RobotDelegate(object):
         time.sleep(0.5)
         self.robot.stop()
         self.robot.arm_up()
+        send_pc_message('Waiting...')
 
     def hands_up(self):
+        send_pc_message('HANDS UP!')
         self.robot.arm_up()
+        send_pc_message('Waiting...')
 
     def arm_down(self):
         self.robot.arm_down()
@@ -85,6 +95,10 @@ def main():
     mqtt_client = com.MqttClient(rd)
     mqtt_client.connect_to_pc()
     rd.loop_forever()
+
+
+def send_pc_message(string):
+    mqtt_client.send_message("display_message", [string])
 
 
 main()
