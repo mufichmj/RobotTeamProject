bfd80d9f2ffaea44824446af65d731309afcd174
mufichmj <mufichmj@rose-hulman.edu>
2018-05-19 15:30:15 -0400

    mufichmj

--- projects/Mariah/Mariah_Project_EV3.py
+++ projects/Mariah/Mariah_Project_EV3.py
@@ -7,11 +7,11 @@ import robot_controller as robo
 class Robot(object):
     def __init__(self):
         self.robot = robo.Snatch3r()
-        # self.pixy = ev3.Sensor(driver_name="pixy_lego")
-        # self.pixy.mode = "SIG1"
-        # print("(X, Y)=({}, {}) Width={} Height={}".format(
-        #     pixy.value(1), pixy.value(2), pixy.value(3),
-        #     pixy.value(4)))
+        self.pixy = ev3.Sensor(driver_name="pixy_lego")
+        self.pixy.mode = "SIG1"
+        print("(X, Y)=({}, {}) Width={} Height={}".format(
+            pixy.value(1), pixy.value(2), pixy.value(3),
+            pixy.value(4)))
 
 
 
@@ -39,11 +39,31 @@ class Robot(object):
             self.robot.spin_left_degrees(90)
 
 
+    def forward(self, left, right):
+        self.left_motor.run_forever(speed_sp=left)
+        self.right_motor.run_forever(speed_sp=right)
 
-    # def shoot_soccer_ball(self, left_speed_entry, right_speed_entry):
-    #     self.forward(left_speed_entry, right_speed_entry)
-    #     while True:
-    #         self.pixy.mode = ""
+
+    def left(self):
+
+
+    def right(self):
+
+
+    def backward(self):
+
+
+    def up(self):
+
+
+    def down(self):
+
+
+
+    def shoot_soccer_ball(self, left_speed_entry, right_speed_entry):
+        self.forward(left_speed_entry, right_speed_entry)
+        while True:
+            self.pixy.mode = ""
 
 
 
